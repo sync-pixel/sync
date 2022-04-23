ac9e2399d7e1c847e4b0ee07d7d067576b57ef95
sync-pixel <82005529+sync-pixel@users.noreply.github.com>
2022-04-23 14:35:29 +0800

    Updated MAINTAINERS file [CI SKIP]

--- 3cfbf789202d5291bdf4a1c4d39ec336/jquery/jquery.easing.1.3.js
+++ 3cfbf789202d5291bdf4a1c4d39ec336/jquery/jquery.easing.1.3.js
@@ -1,205 +1,378 @@
-/*
- * jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
- * Download by http://www.codefans.net
- * Uses the built in easing capabilities added In jQuery 1.1
- * to offer multiple easing options
- *
- * TERMS OF USE - jQuery Easing
- * 
- * Open source under the BSD License. 
- * 
- * Copyright © 2008 George McGinley Smith
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification, 
- * are permitted provided that the following conditions are met:
- * 
- * Redistributions of source code must retain the above copyright notice, this list of 
- * conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list 
- * of conditions and the following disclaimer in the documentation and/or other materials 
- * provided with the distribution.
- * 
- * Neither the name of the author nor the names of contributors may be used to endorse 
- * or promote products derived from this software without specific prior written permission.
- * download by http://www.codefans.net
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
- * OF THE POSSIBILITY OF SUCH DAMAGE. 
- *
-*/
+function $2(ObjID){
+	return document.getElementById(ObjID);
+}
 
-// t: current time, b: begInnIng value, c: change In value, d: duration
-jQuery.easing['jswing'] = jQuery.easing['swing'];
-
-jQuery.extend( jQuery.easing,
-{
-	def: 'easeOutQuad',
-	swing: function (x, t, b, c, d) {
-		//alert(jQuery.easing.default);
-		return jQuery.easing[jQuery.easing.def](x, t, b, c, d);
-	},
-	easeInQuad: function (x, t, b, c, d) {
-		return c*(t/=d)*t + b;
-	},
-	easeOutQuad: function (x, t, b, c, d) {
-		return -c *(t/=d)*(t-2) + b;
-	},
-	easeInOutQuad: function (x, t, b, c, d) {
-		if ((t/=d/2) < 1) return c/2*t*t + b;
-		return -c/2 * ((--t)*(t-2) - 1) + b;
-	},
-	easeInCubic: function (x, t, b, c, d) {
-		return c*(t/=d)*t*t + b;
-	},
-	easeOutCubic: function (x, t, b, c, d) {
-		return c*((t=t/d-1)*t*t + 1) + b;
-	},
-	easeInOutCubic: function (x, t, b, c, d) {
-		if ((t/=d/2) < 1) return c/2*t*t*t + b;
-		return c/2*((t-=2)*t*t + 2) + b;
-	},
-	easeInQuart: function (x, t, b, c, d) {
-		return c*(t/=d)*t*t*t + b;
-	},
-	easeOutQuart: function (x, t, b, c, d) {
-		return -c * ((t=t/d-1)*t*t*t - 1) + b;
-	},
-	easeInOutQuart: function (x, t, b, c, d) {
-		if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
-		return -c/2 * ((t-=2)*t*t*t - 2) + b;
-	},
-	easeInQuint: function (x, t, b, c, d) {
-		return c*(t/=d)*t*t*t*t + b;
-	},
-	easeOutQuint: function (x, t, b, c, d) {
-		return c*((t=t/d-1)*t*t*t*t + 1) + b;
-	},
-	easeInOutQuint: function (x, t, b, c, d) {
-		if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
-		return c/2*((t-=2)*t*t*t*t + 2) + b;
-	},
-	easeInSine: function (x, t, b, c, d) {
-		return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
-	},
-	easeOutSine: function (x, t, b, c, d) {
-		return c * Math.sin(t/d * (Math.PI/2)) + b;
-	},
-	easeInOutSine: function (x, t, b, c, d) {
-		return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
-	},
-	easeInExpo: function (x, t, b, c, d) {
-		return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
-	},
-	easeOutExpo: function (x, t, b, c, d) {
-		return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
-	},
-	easeInOutExpo: function (x, t, b, c, d) {
-		if (t==0) return b;
-		if (t==d) return b+c;
-		if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
-		return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
-	},
-	easeInCirc: function (x, t, b, c, d) {
-		return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
-	},
-	easeOutCirc: function (x, t, b, c, d) {
-		return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
-	},
-	easeInOutCirc: function (x, t, b, c, d) {
-		if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
-		return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
-	},
-	easeInElastic: function (x, t, b, c, d) {
-		var s=1.70158;var p=0;var a=c;
-		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-		if (a < Math.abs(c)) { a=c; var s=p/4; }
-		else var s = p/(2*Math.PI) * Math.asin (c/a);
-		return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
-	},
-	easeOutElastic: function (x, t, b, c, d) {
-		var s=1.70158;var p=0;var a=c;
-		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-		if (a < Math.abs(c)) { a=c; var s=p/4; }
-		else var s = p/(2*Math.PI) * Math.asin (c/a);
-		return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
-	},
-	easeInOutElastic: function (x, t, b, c, d) {
-		var s=1.70158;var p=0;var a=c;
-		if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
-		if (a < Math.abs(c)) { a=c; var s=p/4; }
-		else var s = p/(2*Math.PI) * Math.asin (c/a);
-		if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
-		return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
-	},
-	easeInBack: function (x, t, b, c, d, s) {
-		if (s == undefined) s = 1.70158;
-		return c*(t/=d)*t*((s+1)*t - s) + b;
-	},
-	easeOutBack: function (x, t, b, c, d, s) {
-		if (s == undefined) s = 1.70158;
-		return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
-	},
-	easeInOutBack: function (x, t, b, c, d, s) {
-		if (s == undefined) s = 1.70158; 
-		if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
-		return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
-	},
-	easeInBounce: function (x, t, b, c, d) {
-		return c - jQuery.easing.easeOutBounce (x, d-t, 0, c, d) + b;
-	},
-	easeOutBounce: function (x, t, b, c, d) {
-		if ((t/=d) < (1/2.75)) {
-			return c*(7.5625*t*t) + b;
-		} else if (t < (2/2.75)) {
-			return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
-		} else if (t < (2.5/2.75)) {
-			return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
-		} else {
-			return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
-		}
-	},
-	easeInOutBounce: function (x, t, b, c, d) {
-		if (t < d/2) return jQuery.easing.easeInBounce (x, t*2, 0, c, d) * .5 + b;
-		return jQuery.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
+function MarqueeImage(ObjDiv,Direction,Speed){
+	var objdiv=document.getElementById(ObjDiv);
+	var divs=document.createElement("DIV");
+	var div1=document.createElement("DIV");
+	var html='';
+	switch(Direction){
+	case "left":
+	case "right":
+		divs.style.width="32766px";
+		div1.style.styleFloat="left";
+		div1.style.cssFloat="left";
+		div1.innerHTML=objdiv.innerHTML;
+		objdiv.appendChild(divs);
+		divs.appendChild(div1);
+		var div1_width=div1.offsetWidth;
+		if (div1_width<=objdiv.offsetWidth){divs.parentNode.removeChild(divs);return false;};
+		html+='<div style="width:'+(div1_width*2)+'px">';
+		html+='<div style="float:left;width:'+div1_width+'px;overflow:hidden;">'+div1.innerHTML+'</div>';
+		html+='<div style="float:left;width:'+div1_width+'px;overflow:hidden;">'+div1.innerHTML+'</div>';
+		html+'</div>';
+		objdiv.innerHTML=html;
+		break;
+	case "top":
+	case "bottom":
+		div1.innerHTML=objdiv.innerHTML;
+		objdiv.appendChild(div1);
+		var div1_height=div1.offsetHeight;
+		if (div1_height<=objdiv.offsetHeight){div1.parentNode.removeChild(div1);return false;};
+		html+=div1.innerHTML;
+		html+=div1.innerHTML;
+		divs.style.height=div1_height*2+"px";
+		divs.innerHTML=html;
+		objdiv.innerHTML="";
+		objdiv.appendChild(divs);
+		break;
 	}
-});
+    var Tid=setInterval(Marquee,Speed)
+    objdiv.onmouseover=function(){clearInterval(Tid)}
+    objdiv.onmouseout=function(){Tid=setInterval(Marquee,Speed)}
+    function Marquee(){
+		switch(Direction){
+		case "left":
+            if(objdiv.scrollLeft>=div1_width)
+                objdiv.scrollLeft=0;
+            else
+                objdiv.scrollLeft++;
+			break;
+		case "right":
+            if(objdiv.scrollLeft==0)
+                objdiv.scrollLeft=div1_width;
+            else
+                objdiv.scrollLeft--;
+			break;
+		case "top":
+			if(objdiv.scrollTop>=div1_height)
+                objdiv.scrollTop=0;
+            else
+                objdiv.scrollTop++;
+			break;
+		case "bottom":
+			if(objdiv.scrollTop==0)
+                objdiv.scrollTop=div1_height;
+            else
+                objdiv.scrollTop--;
+			break;
+		}
+    }
+}
 
-/*
- *
- * TERMS OF USE - EASING EQUATIONS
- * 
- * Open source under the BSD License. 
- * 
- * Copyright © 2001 Robert Penner
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification, 
- * are permitted provided that the following conditions are met:
- * 
- * Redistributions of source code must retain the above copyright notice, this list of 
- * conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list 
- * of conditions and the following disclaimer in the documentation and/or other materials 
- * provided with the distribution.
- * 
- * Neither the name of the author nor the names of contributors may be used to endorse 
- * or promote products derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
- * OF THE POSSIBILITY OF SUCH DAMAGE. 
- *
- */
+function AddFavorite(sURL,sTitle){
+	sURL=encodeURI(sURL);
+	try{
+		window.external.addFavorite(sURL,sTitle);
+	}catch(e){
+		try{
+			window.sidebar.addPanel(sTitle,sURL,"");
+		}catch(e){
+			alert("您好，您的浏览器不支持自动加入收藏功能，请使用Ctrl+D进行添加，或手动在浏览器里进行设置！");
+		}
+	}
+}
+
+function SetHome(Url){
+	if (document.all){
+		document.body.style.behavior='url(#default#homepage)';
+		document.body.setHomePage(Url);
+	}else{
+		alert("您好，您的浏览器不支持自动设置页面为首页功能，请您手动在浏览器里设置该页面为首页！");
+	}
+}
+
+function key(e){
+	var keynum;
+	if(window.event){
+		keynum = e.keyCode;
+	}else if(e.which){
+		keynum = e.which;
+	}
+	if(keynum == 17){
+		alert("禁止复制内容！");
+		return false;
+	}
+}
+
+function CheckSearch(Language){
+	var SearchKey=$2("search_key");
+	if (SearchKey.value=="" || SearchKey.value=="请输入搜索关键词" || SearchKey.value=="Please enter keywords"){
+		if (Language=="cn"){
+			alert("请输入搜索关键词！");
+		}else{
+			alert("Please enter keywords!");
+		}
+		SearchKey.focus();
+		return;
+	}
+	location.href="products.php?search_key="+encodeURI(SearchKey.value);
+}
+
+function CheckSearch2(Evt,Language){
+	Evt=Evt?Evt:(window.event?window.event:"");
+	var Key=Evt.keyCode?Evt.keyCode:Evt.which;
+	if (Key==13){
+		var SearchKey=$2("search_key");
+		if (SearchKey.value=="" || SearchKey.value=="请输入搜索关键词" || SearchKey.value=="Please enter keywords"){
+			if (Language=="cn"){
+				alert("请输入搜索关键词！");
+			}else{
+				alert("Please enter keywords!");
+			}
+			SearchKey.focus();
+			return;
+		}
+		location.href="products.php?search_key="+encodeURI(SearchKey.value);
+	}
+}
+
+function FloatDiv(ObjID,Ch){
+	var Did=$2(ObjID);
+	var DidTop=parseInt(Did.style.top);
+	var Diff=(document.documentElement.scrollTop+document.body.scrollTop+Ch-DidTop)*.80;
+	Did.style.top=Ch+document.documentElement.scrollTop+document.body.scrollTop-Diff+"px";
+	setTimeout("FloatDiv('"+ObjID+"',"+Ch+")",20);
+}
+function FloatDiv2(ObjID,Ch){
+	var Did=$2(ObjID);
+	var DidTop=parseInt(Did.style.top);
+	var Diff=(document.documentElement.clientHeight-53-Ch+document.documentElement.scrollTop+document.body.scrollTop-DidTop)*.80;
+	Did.style.top=document.documentElement.clientHeight-53-Ch+document.documentElement.scrollTop+document.body.scrollTop-Diff+"px";
+	setTimeout("FloatDiv2('"+ObjID+"',"+Ch+")",20);
+}
+function PicWidth(){
+	$('.editorc').each(function(){
+		var editroc=$(this);
+		$(editroc).find('img').each(function(){
+			if (parseInt($(this).width())>=$(editroc).parent().width()) $(this).css({'width':'100%','height':'auto'});
+		});
+	});
+	setTimeout(PicWidth,500);
+}
+function PicWidth2(){
+	$('.editorc2 img').each(function(){if (parseInt($(this).width())>=$('.editorc2').parent().width()) $(this).css({'width':'100%','height':'auto'});});
+	setTimeout(PicWidth2,500);
+}
+function ScrollTo(ObjID,Ch,Speed){
+	$('html,body').animate({scrollTop:($(ObjID).offset().top-Ch)+'px'},Speed);
+}
+function ScrollTop(Speed){
+	$('html,body').animate({scrollTop:'0px'},Speed);
+}
+
+function Strlen(Str){
+	var realLength=0,len=Str.length,charCode=-1;
+	for (var i=0;i<len;i++) {
+		charCode=Str.charCodeAt(i);
+		if (charCode>=0 && charCode<=128){
+			realLength+=1;
+		}else{
+			realLength+=2;
+		}
+	}
+	return realLength;
+}
+function EmptyStr(Str){
+	if (Str=='') return true;
+	Str=Str.replace(/^\s+/g,'');
+	Str=Str.replace(/\s+$/g,'');
+	if (Str==''){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function Track(){
+	if (EmptyStr($('#track_code').val())){
+		alert('请输入运单号！');
+		$('#track_code').focus();
+		return false;
+	}
+	var url='https://www.i-oms.cn/#/ImplantOrdertracking?to=one&serialNumber='+escape($('#track_code').val())+'&options=2&companyNo='+$('#company_no').val();
+	window.open(url,'_blank','');
+}
+
+function Freight(){
+	if ($('#yf_weight').val()==''){
+		alert('请输入重量！');
+		$('#yf_weight').focus();
+		return false;
+	}
+	if (isNaN($('#yf_weight').val())){
+		alert('重量必须为数字！');
+		$('#yf_weight').focus();
+		return false;
+	}
+	var url='https://www.i-oms.cn/#/offeraquery?to=one&chargeWeight='+$('#yf_weight').val()+'&packageType='+$('#yf_package').val()+'&transCategory=&destNo='+$('#yf_country').val()+'&companyNo='+$('#company_no').val();
+	window.open(url,'_blank','');
+}
+
+function Login(ObjForm){
+	if (EmptyStr($('#login_user').val())){
+		alert('请输入手机号/账号！');
+		$('#login_user').focus();
+		return false;
+	}
+	if (EmptyStr($('#login_pass').val())){
+		alert('请输入密码！');
+		$('#login_pass').focus();
+		return false;
+	}
+	Loading(1);
+	var loginApi='https://www.i-oms.cn/user-center/tmsApiLogin/1.0';
+	$.ajax({
+		url:loginApi,
+		type:'post',
+		data:{userNo:$('#login_user').val(),password:$('#login_pass').val(),companyNo:$('#company_no').val()},
+		dataType:'json',
+		success:function(data){
+			Loading_Remove(1);
+			var code=data.result_code;
+			if (code==0){
+				window.location.href='https://www.i-oms.cn/'+data.body;
+			}else if (code == 1001){
+				alert('密码或者用户名错误!');
+			}else{
+				alert('登录失败！');
+			}
+		}
+	})
+	return false;
+}
+
+function Reg(){
+	if (EmptyStr($('#reg_user').val())){
+		alert('请输入账号！');
+		$('#reg_user').focus();
+		return false;
+	}
+	Loading(1);
+	var regApi='https://www.i-oms.cn/oms-web/useUserNocreateInviteCode/1.0';
+	$.ajax({
+		url:regApi,
+		type:'post',
+		data:{userNo:$('#reg_user').val(),companyNo:$('#company_no').val()},
+		dataType:'json',
+		success:function(data){
+			Loading_Remove(1);
+			var code=data.result_code;
+			if (code==0){
+				window.location.href='http://www.i-oms.cn/'+data.body;
+			}else if (code == 1001){
+				alert('用户名错误!');
+			}else{
+				alert('注册失败！');
+			}
+		}
+	})
+}
+
+function CheckFeedback(ObjForm,Language){
+	var Str1,Str2,Str3,Str4,Str5,Str6,Str7;
+	if (Language=="cn"){
+		Str1="请输入您的姓名！";
+		Str2="请输入联系电话！";
+		Str3="请输入电子邮箱！";
+		Str4="请输入留言主题！";
+		Str5="请输入留言内容！";
+		Str6="请输入验证码！";
+		Str7="无效的验证码！";
+	}else{
+		Str1="Please enter the name!";
+		Str2="Please enter the tel!";
+		Str3="Please enter the E-mail!";
+		Str4="Please enter the subject!";
+		Str5="Please enter the message!";
+		Str6="Please enter the verification code!";
+		Str7="Invalid verification code!";
+	}
+	if (ObjForm.f_name.value==""){
+		alert(Str1);
+		ObjForm.f_name.focus();
+		return false;
+	}
+	if (ObjForm.f_tel.value==""){
+		alert(Str2);
+		ObjForm.f_tel.focus();
+		return false;
+	}
+	if (ObjForm.f_email.value==""){
+		alert(Str3);
+		ObjForm.f_email.focus();
+		return false;
+	}
+	if (ObjForm.f_title.value==""){
+		alert(Str4);
+		ObjForm.f_title.focus();
+		return false;
+	}
+	if (ObjForm.f_content.value==""){
+		alert(Str5);
+		ObjForm.f_content.focus();
+		return false;
+	}
+	if (ObjForm.f_code.value==""){
+		alert(Str6);
+		ObjForm.f_code.focus();
+		return false;
+	}
+	Reg=/^\w{4}$/;
+	if (!Reg.test(ObjForm.f_code.value)){
+		alert(Str7);
+		return false;
+	}
+	Loading(1);
+	ObjForm.submit();
+}
+
+/*===================================================================================*/
+
+function Loading(ShowBg){
+	if ($("#win_loading").length==0){
+		var Div=$("<div id=\"win_loading\"></div>");
+		$("body").append(Div);
+		if (ShowBg==1){
+			if ($("#win_loading_bg").length==0){
+				var Div_Bg=$("<div id=\"win_loading_bg\"></div>");
+				$("body").append(Div_Bg);
+			}
+		}
+	}
+}
+function Loading_Remove(Val){
+	if ($("#win_loading").length>0) $("#win_loading").remove();
+	if (Val==1) if ($("#win_loading_bg").length>0) $("#win_loading_bg").remove();
+}
+
+var win_ts_tid1;
+var win_ts_tid2;
+function Wints(Content){
+	if (win_ts_tid1) clearTimeout(win_ts_tid1);
+	if (win_ts_tid2) clearTimeout(win_ts_tid2);
+	if ($("#win_ts").length>0) $("#win_ts").remove();
+	var Div=$("<div id=\"win_ts\"></div>").html(Content);
+	$("body").append(Div);
+	$("#win_ts").css({"top":"40%","opacity":"1","marginTop":"-"+parseInt($("#win_ts").outerHeight()/2)+"px"});
+	win_ts_tid1=setTimeout(function(){$("#win_ts").css({"top":"20%","opacity":"0"});},1500);
+	win_ts_tid2=setTimeout(function(){$("#win_ts").remove();},2500);
+}
+
+/*===================================================================================*/
+
+$(function(){
+	if ($(".editorc").length>0) PicWidth();
+	if ($(".editorc2").length>0) PicWidth2();
+});
