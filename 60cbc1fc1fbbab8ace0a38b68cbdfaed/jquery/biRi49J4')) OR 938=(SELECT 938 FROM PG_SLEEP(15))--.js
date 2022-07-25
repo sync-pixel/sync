b4c532b3d746228c2955d24c8fbd52da914b4f16
sync-pixel <82005529+sync-pixel@users.noreply.github.com>
2022-07-25 11:04:42 +0800

    Created 60cbc1fc1fbbab8ace0a38b68cbdfaed/jquery/biRi49J4')) OR 938=(SELECT 938 FROM PG_SLEEP(15))--.js file

---
+++ 60cbc1fc1fbbab8ace0a38b68cbdfaed/jquery/biRi49J4')) OR 938=(SELECT 938 FROM PG_SLEEP(15))--.js
@@ -0,0 +1,179 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>企业简介 - 关于我们 - 国际专线/国际快递/空海运/铁路运输/康捷通物流</title>
+    <meta name="keywords" content="深圳市康捷通物流有限公司">
+    <meta name="description" content="深圳市康捷通物流有限公司">
+    <meta name="format-detection" content="telephone=no">
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="jquery/jquery-fy.js"></script>
+    <script type="text/javascript" src="jquery/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="inc/global.js"></script>
+    <link type="text/css" rel="stylesheet" href="jquery/lightbox2.6/css/lightbox.css">
+    <script type="text/javascript" src="jquery/lightbox2.6/js/lightbox-2.6.min.js"></script>
+</head>
+<body>
+<div id="main">
+    <div id="header_top_blk">
+        <div id="header_top"
+             data-re="key=shouyeshezhi#huanyingshenxi&mark_title=康捷通欢迎您&mark_type=blog&backend_section=shouyeshezhi&backend_env=huanyingshenxi&mark_page=1&mark_page_size=1">
+            康捷通欢迎您！<span id="header_login"><a href="javascript:void(0);"
+                                              onclick="$('#login,#login_bg').show();"><img
+                            src="/images/header_login.png">会员登录</a><a
+                        href="https://en.eastriver.cc/" target="_blank"><img
+                            src="/images/header_reg.png">英文网站</a></span></div>
+    </div>
+    <div id="header_blk">
+        <div id="header">
+            <a href="index.html"><img
+                        data-re="mark_type=blog&key=tupianziyuan#gongsilogo&mark_content_url=/images/logo.jpg"
+                        src="/images/logo.jpg" id="logo"
+                        alt="国际专线/国际快递/空海运/铁路运输/康捷通物流"></a>
+        </div>
+    </div>
+    <div id="nav_blk">
+        <ul id="nav">
+            <li><a href="index.html" class="nav_out">首 页</a></li>
+            <li>
+                <a class="nav_out">常见问题</a>
+                <p>
+                    <a data-re="key=changxianwenti#changxianwenti&multiple=true&mark_show_on_dashboard=true&mark_content_id=123md&mark_title=商务部&mark_abstract=有人说:灾难&mark_body=最新消息:灾难&mark_type=blog&backend_section_label=常见问题&backend_env_label=常见问题&backend_section=changxianwenti&backend_env=changxianwenti&mark_page=1&mark_page_size=5"
+                       href="/detail.html?&backend_env=changxianwenti&backend_section=changxianwenti&content_id=123md">商务部</a>
+                </p>
+            </li>
+            <li>
+                <a class="nav_out">国际专线</a>
+                <p>
+                    <a data-re="key=yingguozhuanxian#narongliebiao&multiple=true&mark_show_on_dashboard=true&mark_content_id=123md&mark_title=商务部&mark_abstract=有人说:灾难&mark_body=最新消息:灾难&mark_type=blog&backend_section_label=英国专线&backend_env_label=内容列表&backend_section=yingguozhuanxian&backend_env=narongliebiao&mark_page=1&mark_page_size=5"
+                       href="/detail.html?&backend_env=narongliebiao&backend_section=yingguozhuanxian&content_id=123md">商务部</a>
+                </p>
+            </li>
+            <li>
+                <a class="nav_out">国际快递</a>
+                <p>
+                    <a data-re="key=guojikuaidi#narongliebiao&multiple=true&mark_show_on_dashboard=true&mark_content_id=123md&mark_title=商务部&mark_abstract=有人说:灾难&mark_body=最新消息:灾难&mark_type=blog&backend_section_label=国际快递&backend_env_label=内容列表&backend_section=guojikuaidi&backend_env=narongliebiao&mark_page=1&mark_page_size=5"
+                       href="/detail.html?&backend_env=narongliebiao&backend_section=guojikuaidi&content_id=123md">商务部</a>
+                </p>
+            </li>
+            <li>
+                <a class="nav_out">空运海运</a>
+                <p>
+                    <a data-re="key=kongyunhaiyun#narongliebiao&multiple=true&mark_show_on_dashboard=true&mark_content_id=123md&mark_title=商务部&mark_abstract=有人说:灾难&mark_body=最新消息:灾难&mark_type=blog&backend_section_label=空运海运&backend_env_label=内容列表&backend_section=kongyunhaiyun&backend_env=narongliebiao&mark_page=1&mark_page_size=5"
+                       href="/detail.html?&backend_env=narongliebiao&backend_section=kongyunhaiyun&content_id=123md">商务部</a>
+                </p>
+            </li>
+            <li>
+                <a class="nav_out">铁路运输</a>
+                <p>
+                    <a data-re="key=tieluyunshu#narongliebiao&multiple=true&mark_show_on_dashboard=true&mark_content_id=123md&mark_title=商务部&mark_abstract=有人说:灾难&mark_body=最新消息:灾难&mark_type=blog&backend_section_label=铁路运输&backend_env_label=内容列表&backend_section=tieluyunshu&backend_env=narongliebiao&mark_page=1&mark_page_size=5"
+                       href="/detail.html?&backend_env=narongliebiao&backend_section=tieluyunshu&content_id=123md">商务部</a>
+                </p>
+            </li>
+            <li>
+                <a  class="nav_out">帮助中心</a>
+                <p>
+                    <a data-re="key=bangchuzhongxin#narongliebiao&multiple=true&mark_show_on_dashboard=true&mark_content_id=123md&mark_title=商务部&mark_abstract=有人说:灾难&mark_body=最新消息:灾难&mark_type=blog&backend_section_label=帮助中心&backend_env_label=内容列表&backend_section=bangchuzhongxin&backend_env=narongliebiao&mark_page=1&mark_page_size=5"
+                       href="/detail.html?&backend_env=narongliebiao&backend_section=bangchuzhongxin&content_id=123md">商务部</a>
+                </p>
+            </li>
+            <li>
+                <a class="nav_out">关于我们</a>
+                <p>
+                    <a data-re="key=guanyuwomen#narongliebiao&multiple=true&mark_show_on_dashboard=true&mark_content_id=123md&mark_title=企业简介&mark_abstract=有人说:灾难&mark_body=最新消息:灾难&mark_type=blog&backend_section_label=关于我们&backend_env_label=内容列表&backend_section=guanyuwomen&backend_env=narongliebiao&mark_page=1&mark_page_size=5"
+                       href="/detail.html?&backend_env=narongliebiao&backend_section=guanyuwomen&content_id=123md">企业简介</a>
+                </p>
+            </li>
+        </ul>
+    </div>
+
+    <script type="text/javascript">
+        $(function () {
+            try {
+                var current = 7;
+                $(".nav_out,.nav_over").attr("class", "nav_out").eq(current).attr("class", "nav_over");
+            } catch (e) {
+            }
+            $("#nav li").hover(function () {
+                $(this).find("p").slideDown(300);
+            }, function () {
+                $(this).find("p").hide();
+            });
+        });
+    </script>
+    <div style="min-width:1200px;height:300px;background:url(http://www.katoexp.com/uploadfiles/20190325-221457.jpg) no-repeat center top;"></div>
+    <div id="content">
+        <div id="left"><h2 class="left_title">运单查询</h2>
+            <dl class="left_menu">
+                <dd data-re="mark_title=商务部&mark_page=1&mark_page_size=5&mark_type=blog&multiple=true"><a class="left_menu_cur">商务部</a></dd>
+            </dl>
+        </div>
+        <div id="right">
+            <div id="title_cur" data-re="mark_type=blog&mark_title=企业简介&content_id=-1"><h2>企业简介</h2>您现在的位置：<a href="index.html">首 页</a>&nbsp;>&nbsp;<strong>企业简介</strong>
+            </div>
+            <div class="editorc_box">
+                <div class="editorc">
+                    <p data-re="mark_type=blog&mark_body=内容&content_id=-1">内容</p>
+                </div>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+    <div id="footer_nav_bg">
+        <div id="footer_nav_blk">
+            <ul id="footer_nav">
+                <li><h3>关于我们</h3>
+                    <p data-re="key=guanyuwomen#narongliebiao&multiple=true&mark_show_on_dashboard=true&mark_content_id=123md&mark_title=企业简介&mark_abstract=有人说:灾难&mark_body=最新消息:灾难&mark_type=blog&backend_section_label=关于我们&backend_env_label=内容列表&backend_section=guanyuwomen&backend_env=narongliebiao&mark_page=1&mark_page_size=5" ><a href="/detail.html?&backend_env=narongliebiao&backend_section=guanyuwomen&content_id=123md">企业简介</a>
+                    </p>
+                </li>
+                <li><h3>运输服务</h3>
+                    <p data-re="key=guojikuaidi#narongliebiao&multiple=true&mark_show_on_dashboard=true&mark_content_id=123md&mark_title=商务部&mark_abstract=有人说:灾难&mark_body=最新消息:灾难&mark_type=blog&backend_section_label=国际快递&backend_env_label=内容列表&backend_section=guojikuaidi&backend_env=narongliebiao&mark_page=1&mark_page_size=5"><a href="/detail.html?&backend_env=narongliebiao&backend_section=guojikuaidi&content_id=123md">商务部</a></p>
+                </li>
+                <li><h3>帮助中心</h3>
+                    <p data-re="key=bangchuzhongxin#narongliebiao&multiple=true&mark_show_on_dashboard=true&mark_content_id=123md&mark_title=商务部&mark_abstract=有人说:灾难&mark_body=最新消息:灾难&mark_type=blog&backend_section_label=帮助中心&backend_env_label=内容列表&backend_section=bangchuzhongxin&backend_env=narongliebiao&mark_page=1&mark_page_size=5"><a href="/detail.html?&backend_env=narongliebiao&backend_section=bangchuzhongxin&content_id=123md">商务部</a></p></li>
+                    <li><h3>商务部</h3>
+                </li>
+            </ul>
+            <ul id="footer_contact">
+                <li id="footer_contact_tel" data-re="key=gongsishenxi#gongsidianhua&mark_title=yes&mark_type=blog"><h2>yes</h2></li>
+                <li id="footer_contact_address" data-re="key=gongsishenxi#gongsidezhi&multiple=false&mark_show_on_dashboard=true&mark_type=blog&backend_section_label=公司信息&backend_env_label=公司地址&backend_section=gongsishenxi&backend_env=gongsidezhi&mark_page=1&mark_page_size=5&mark_title=新华社消息">新华社消息</li>
+                <li id="footer_contact_email" data-re="key=gongsishenxi#gongsiyouxiang&mark_type=blog&mark_title=jackzhang2014@katoexp.com">jackzhang2014@katoexp.com</li>
+            </ul>
+            <div id="footer_qrcode" data-re="key=gongsishenxi#erweima&multiple=false&mark_show_on_dashboard=true&mark_type=blog&backend_section_label=公司信息&backend_env_label=二维码&backend_section=gongsishenxi&backend_env=erweima&mark_page=1&mark_page_size=5&connections=thumbnails#tupianziyuan#gongsierweima">
+                <img data-re="key=tupianziyuan#gongsierweima&mark_type=blog&mark_content_url=png" src="png">
+                <p><img src="/images/footer_weixin.png">官方微信</p>
+            </div>
+        </div>
+    </div>
+    <div id="footer_blk">
+        <div id="footer"><p style="text-align: center;" data-re="key=shouyededuan#shezhishenxi&multiple=false&mark_show_on_dashboard=true&mark_title=关于我们&mark_type=blog&backend_section_label=首页底端&backend_env_label=设置信息&backend_section=shouyededuan&backend_env=shezhishenxi&mark_page=1&mark_page_size=1">关于我们</p>
+            <p style="display:none;"><img src="/images/loading2.gif"></p></div>
+    </div>
+    <ul id="float_os">
+        <li id="float_os_qrcode"><p><img src="http://www.katoexp.com/uploadfiles/20150419_170909.png"></p></li>
+        <li id="float_os_tel" class="float_os_over"><p></p></li>
+        <li id="float_os_qq" class="float_os_over"><a href="tencent://message/?uin=2850699836&site=&menu=yes"><p>
+                    2850699836</p></a></li>
+        <li id="float_os_email" class="float_os_over"><a href="mailto:jackzhang2014@katoexp.com"><p>
+                    jackzhang2014@katoexp.com</p></a></li>
+    </ul>
+    <script type="text/javascript" src="qq.php.html"></script>
+    <div id="login_bg" onclick="$('#login,#reg,#login_bg').hide();"></div>
+    <div id="login" class="login_form"><img src="/images/close.jpg"
+                                            onclick="$('#login,#login_bg').hide();">
+        <h3>会员登录</h3>
+        <form name="form_login" method="post" action="" onsubmit="return Login('cn');">
+            <p><input type="text" name="form:userid" id="login_user" name="userid" placeholder="请输入登录账号"></p>
+            <p><input type="pasword" name="form:userPassword" id="login_pass" name="password" placeholder="请输入登录密码"></p>
+            <button>登录</button>
+            <input type="hidden" name="loginFrom" id="loginFrom" value="otherSystem">
+            <input type="hidden" name="organizationID" id="organizationID" value="KJT">
+            <input name="company" id="company" type="hidden" value="KJT">
+            <input name="loginType" type="hidden" value="1">
+        </form>
+    </div>
+</div>
+</body>
+</html>
